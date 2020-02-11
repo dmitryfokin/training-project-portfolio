4a6f2fb3b2f03df81c865a1033ab934f70018d31
Dmitry Fokin <dkoder.dvl@gmail.com>
2020-02-11 23:38:16 +0300

    create docs for page on github

--- webpack.config.js
+++ webpack.config.js
@@ -5,67 +5,67 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-	entry: {
-		main: path.resolve(__dirname, 'src', 'app.js'),
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'js/main.js',
-	},
-	devServer: {
-		port: 4200,
-		host: 'localhost',
-		contentBase: path.resolve(__dirname, 'dist'),
-		open: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.pug$/,
-				loader: "pug-loader",
-				options: {
-					pretty: true,
-				},
-			},
-			{
-				test: /\.less$/,
-				use: [{loader: MiniCSSExtractPlugin.loader},
-					{
-						loader: 'css-loader',
-						options: {
-							url: false,
-							sourceMap: true,
-						}
-					},
-					{
-						loader: 'less-loader',
-						options: {
-							relativeUrls: false,
-							sourceMap: true,
-						}
-					},
-				]
-			}
-		]
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new CopyWebpackPlugin([
-			{
-				from: path.resolve(__dirname, 'src', 'img'),
-				to: path.resolve(__dirname, 'dist', 'img')
-			},
-			{
-				from: path.resolve(__dirname, 'src', 'libs'),
-				to: path.resolve(__dirname, 'dist', 'libs')
-			}
-		]),
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'src', 'pug', 'pages', 'index.pug'),
-			filename: path.resolve(__dirname, 'dist', 'index.html')
-		}),
-		new MiniCSSExtractPlugin({
-			filename: `./css/[name].css`
-		})
-	]
+  entry: {
+    main: path.resolve(__dirname, 'src', 'app.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/main.js',
+  },
+  devServer: {
+    port: 4200,
+    host: 'localhost',
+    contentBase: path.resolve(__dirname, 'dist'),
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: "pug-loader",
+        options: {
+          pretty: true,
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [{loader: MiniCSSExtractPlugin.loader},
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              sourceMap: true,
+            }
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              relativeUrls: false,
+              sourceMap: true,
+            }
+          },
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, 'src', 'img'),
+        to: path.resolve(__dirname, 'dist', 'img')
+      },
+      {
+        from: path.resolve(__dirname, 'src', 'libs'),
+        to: path.resolve(__dirname, 'dist', 'libs')
+      },
+    ]),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'pug', 'pages', 'index.pug'),
+      filename: path.resolve(__dirname, 'dist', 'index.html')
+    }),
+    new MiniCSSExtractPlugin({
+      filename: `./css/[name].css`
+    })
+  ]
 }
